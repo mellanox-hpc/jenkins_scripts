bd9789a088193d9fa99faadd361292564829439d
Mike Dubman <miked@mellanox.com>
2015-11-17 20:37:19 +0200

    ompi: add trace origin to vg

--- jenkins/ompi/ompi_jenkins.sh
+++ jenkins/ompi/ompi_jenkins.sh
@@ -690,7 +690,7 @@ if [ -n "$JENKINS_RUN_TESTS" ]; then
             module load mlnx-hpc-latest/gcc/stack
 
             exe_dir=$OMPI_HOME/examples
-            vg_opt="--suppressions=$OMPI_HOME/share/openmpi/openmpi-valgrind.supp --suppressions=$abs_path/vg.supp --error-exitcode=3"
+            vg_opt="--suppressions=$OMPI_HOME/share/openmpi/openmpi-valgrind.supp --suppressions=$abs_path/vg.supp --error-exitcode=3 --track-origins=yes"
             mpi_opt="-mca coll ^hcoll -np 1"
 
             mpi_exe=$OMPI_HOME/examples/hello_c
