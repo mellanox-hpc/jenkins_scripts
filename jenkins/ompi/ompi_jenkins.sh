52e8b84ef180529b8052d82023df0d034ba3f1b4
Artem Polyakov <artpol84@gmail.com>
2017-07-17 22:49:46 +0700

    ompi: enable opeib testing on v2.x

--- jenkins/ompi/ompi_jenkins.sh
+++ jenkins/ompi/ompi_jenkins.sh
@@ -61,10 +61,10 @@ if [ "$ghprbTargetBranch" != "v2.x" ] && [ "$ghprbTargetBranch" != "v2.0.x" ] &&
     btl_sm="no"
 fi
 
-if [ "$ghprbTargetBranch" = "v2.x" ]; then
+#if [ "$ghprbTargetBranch" = "v2.x" ]; then
     # temp disable of btl/openib for v2.x branch
-    btl_openib="no"
-fi
+#    btl_openib="no"
+#fi
 
 btl_tcp_bkp=$btl_tcp
 btl_sm_bkp=$btl_sm
